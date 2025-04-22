62e42bb199a3308e3aaa1ef908c5e24f194fe886
Shubham <shubhamshinde.link@gmail.com>
2025-04-22 21:31:47 +0530

    Initial commit

---
+++ src/test/java/com/app/exceltodb/ExceltodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.exceltodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceltodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
